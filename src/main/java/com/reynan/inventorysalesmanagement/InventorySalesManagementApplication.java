744ed2bbecf5dcd0b0cceb7d7f3be94da64f3a91
Reynan <reynandan11@gmail.com>
2026-02-20 13:55:54 -0300

    feat: add initial project skeleton with pom.xml and mvc structure

---
+++ src/main/java/com/reynan/inventorysalesmanagement/InventorySalesManagementApplication.java
@@ -0,0 +1,13 @@
+package com.reynan.inventorysalesmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InventorySalesManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InventorySalesManagementApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/reynan/inventorysalesmanagement/InventorySalesManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.reynan.inventorysalesmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventorySalesManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
