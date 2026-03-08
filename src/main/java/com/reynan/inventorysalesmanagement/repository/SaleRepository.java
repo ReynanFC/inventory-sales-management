de303204ddbae889c885278e519bbb8a7cd2c284
Reynan <reynandan11@gmail.com>
2026-03-08 20:10:11 -0300

    refactor: adjust pagination in services

--- src/main/java/com/reynan/inventorysalesmanagement/repository/SaleRepository.java
+++ src/main/java/com/reynan/inventorysalesmanagement/repository/SaleRepository.java
@@ -1,10 +1,10 @@
 package com.reynan.inventorysalesmanagement.repository;
 
 import com.reynan.inventorysalesmanagement.entities.Sale;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
-
 public interface SaleRepository extends JpaRepository<Sale,Long> {
-    List<Sale> findByCustomerId(Long customerId);
+    Page<Sale> findByCustomerId(Long customerId, Pageable pageable);
 }
