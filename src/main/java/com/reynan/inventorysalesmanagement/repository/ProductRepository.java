ffc730940f6f831fab80c97377448d9ac637f3b0
Reynan <reynandan11@gmail.com>
2026-03-08 18:47:42 -0300

    feat: creation of product filter by category

--- src/main/java/com/reynan/inventorysalesmanagement/repository/ProductRepository.java
+++ src/main/java/com/reynan/inventorysalesmanagement/repository/ProductRepository.java
@@ -1,7 +1,10 @@
 package com.reynan.inventorysalesmanagement.repository;
 
 import com.reynan.inventorysalesmanagement.entities.Product;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface ProductRepository extends JpaRepository<Product,Long> {
+    Page<Product> findByCategoryId(Long categoryId, Pageable pageable);
 }
