ac5261939d91d3b197f188055fa4d19eafd92d7a
Reynan <reynandan11@gmail.com>
2026-03-08 20:19:20 -0300

    refactor: remove toSet in customer, product e sale mapper

--- src/main/java/com/reynan/inventorysalesmanagement/mapper/ProductMapper.java
+++ src/main/java/com/reynan/inventorysalesmanagement/mapper/ProductMapper.java
@@ -1,5 +1,6 @@
 package com.reynan.inventorysalesmanagement.mapper;
 
+import com.reynan.inventorysalesmanagement.dtos.request.ProductRequestDTO;
 import com.reynan.inventorysalesmanagement.dtos.response.ProductDetailResponseDTO;
 import com.reynan.inventorysalesmanagement.dtos.response.ProductResponseDTO;
 import com.reynan.inventorysalesmanagement.entities.Product;
@@ -7,8 +8,6 @@ import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ReportingPolicy;
-
-import java.util.Set;
 
 @Mapper(componentModel = MappingConstants.ComponentModel.SPRING,
         unmappedTargetPolicy = ReportingPolicy.ERROR)
@@ -20,13 +19,11 @@ public interface ProductMapper {
     @Mapping(target = "quantityInStock", ignore = true)
     @Mapping(target = "stockMovements", ignore = true)
     @Mapping(target = "category", ignore = true)
-    Product toEntity(ProductDetailResponseDTO requestDTO);
+    Product toEntity(ProductRequestDTO requestDTO);
 
     @Mapping(target = "categoryId", source = "category.id")
     @Mapping(target = "categoryName", source = "category.name")
     ProductDetailResponseDTO toDetailResponseDTO(Product entity);
 
     ProductResponseDTO toResponseDTO(Product entity);
-
-    Set<ProductResponseDTO> toSetResponseDTO(Set<Product> entities);
 }
